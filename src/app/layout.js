ca7383884c0ec2b61818e310d0af3712c4121fa8
madedev <nmaderama@gmail.com>
2025-07-16 23:58:00 +0700

    fix meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,16 @@
-import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import './globals.css';
 
 // Konfigurasi font Plus Jakarta Sans
 const jakarta = Plus_Jakarta_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-jakarta", // Gunakan CSS Variable
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-jakarta', // Gunakan CSS Variable
 });
 
 export const metadata = {
-  title: "Sentinel Forces - Keamanan Profesional",
-  description:
-    "Layanan bodyguard, pengamanan event, dan outsourcing security terpercaya.",
+  title: 'TJM Auto Care',
+  description: 'Bengkel Nomor 1 di Indonesia.',
 };
 
 export default function RootLayout({ children }) {
